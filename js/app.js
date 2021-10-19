31b2e2ecc0fb550ff0e40e9ff0ef2bd65cd0af58
Martin <martinstudyweb@gmail.com>
2021-10-19 20:15:42 +0300

    created in html header and about section, styles for them and js function

---
+++ js/app.js
@@ -0,0 +1,36 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector('#nav-menu'),
+        navToggle = document.querySelector('#nav-toggle'),
+        navClose = document.querySelector('#nav-close');
+
+// show menu
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+// hidden menu 
+if (navClose) {
+    navClose.addEventListener('click', linkAction);
+}
+
+/*=============== REMOVE MENU WHEN CLICK LINKS ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(link => link.addEventListener('click', linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.querySelector('#header');
+    // When scroll is greater than 100 viewport height
+    // add the scroll-header class to the head
+    if (this.scrollY >= 100) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
