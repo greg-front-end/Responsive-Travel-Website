8be2f314abce034b81304577d8310622620ada7f
Martin <martinstudyweb@gmail.com>
2021-10-20 21:05:52 +0300

    craeted section video and styles and js function for it

--- js/app.js
+++ js/app.js
@@ -47,3 +47,36 @@ var swiper = new Swiper(".discover__container", {
       rotate: 50,
     },
   });
+
+  /*=============== VIDEO ===============*/
+  const videoFile = document.querySelector('#video-file'),
+        videoBtn = document.querySelector('#video-button'),
+        videoIcon = document.querySelector('#video-icon');
+    
+function playPause() {
+    if (videoFile.paused) {
+        // Play video
+        videoFile.play();
+
+        // If paly, change the icon play
+        videoIcon.classList.add('ri-pause-line');
+        videoIcon.classList.remove('ri-play-line');
+    } else {
+        // pause video 
+        videoFile.pause();
+
+        // If pause, change the icon play
+        videoIcon.classList.add('ri-play-line');
+        videoIcon.classList.remove('ri-pause-line');
+    }
+}
+
+videoBtn.addEventListener('click', playPause);
+
+function finalVideo() {
+    // If endend, change the icon play
+    videoIcon.classList.remove('ri-pause-line');
+    videoIcon.classList.add('ri-play-line');
+}
+
+videoFile.addEventListener('ended', finalVideo);
