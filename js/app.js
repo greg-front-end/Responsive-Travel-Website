143e472e44a5b30d527f0a13baa98dce7aed04fa
Martin <martinstudyweb@gmail.com>
2021-10-22 16:44:27 +0300

    created scroll up button and function for it, and add style for scroll bar, and active links for nav menu

--- js/app.js
+++ js/app.js
@@ -80,3 +80,41 @@ function finalVideo() {
 }
 
 videoFile.addEventListener('ended', finalVideo);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.querySelector('#scroll-up');
+    // when the scroll is higher than 200 viewport height,
+    //  add the show-scroll style
+    if (this.scrollY >= 200) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+window.addEventListener('scroll', scrollUp);
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 120;
+        const sectionId = current.getAttribute('id');
+        /**
+         * srollY = 0
+         * sectionTop = -60, 569, 1350, 3032
+         * sectionHeight = equal height every section
+         * current.offsetTop = every section is below the top in px
+         *  */ 
+        if(sectionId && scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
